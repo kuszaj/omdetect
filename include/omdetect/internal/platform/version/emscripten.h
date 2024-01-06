8e30a776d94edabc1843515054e1d1ce2665d245
kuszaj <kuszaj@users.noreply.github.com>
2024-01-06 17:06:43 +0100

    platform: check if suplemental consts defined

--- include/omdetect/internal/platform/version/emscripten.h
+++ include/omdetect/internal/platform/version/emscripten.h
@@ -6,5 +6,13 @@
 #endif
 
 #if defined(__EMSCRIPTEN_major__)
-    #define OMD_PLATFORM_VERSION OMD_VERSION(__EMSCRIPTEN_major__, __EMSCRIPTEN_minor__, __EMSCRIPTEN_tiny__)
+    #if defined(__EMSCRIPTEN_minor__)
+        #if defined(__EMSCRIPTEN_tiny__)
+            #define OMD_PLATFORM_VERSION OMD_VERSION(__EMSCRIPTEN_major__, __EMSCRIPTEN_minor__, __EMSCRIPTEN_tiny__)
+        #else
+            #define OMD_PLATFORM_VERSION OMD_VERSION(__EMSCRIPTEN_major__, __EMSCRIPTEN_minor__, 0)
+        #endif
+    #else
+        #define OMD_PLATFORM_VERSION OMD_VERSION(__EMSCRIPTEN_major__, 0, 0)
+    #endif
 #endif
--- include/omdetect/internal/platform/version/vxworks.h
+++ include/omdetect/internal/platform/version/vxworks.h
@@ -3,5 +3,13 @@
 
 #include <version.h>
 #if defined(_WRS_VXWORKS_MAJOR)
-    #define OMD_PLATFORM_VERSION OMD_VERSION(_WRS_VXWORKS_MAJOR, _WRS_VXWORKS_MINOR, _WRS_VXWORKS_MAINT)
+    #if defined(_WRS_VXWORKS_MINOR)
+        #if defined(_WRS_VXWORKS_MAINT)
+            #define OMD_PLATFORM_VERSION OMD_VERSION(_WRS_VXWORKS_MAJOR, _WRS_VXWORKS_MINOR, _WRS_VXWORKS_MAINT)
+        #else
+            #define OMD_PLATFORM_VERSION OMD_VERSION(_WRS_VXWORKS_MAJOR, _WRS_VXWORKS_MINOR, 0)
+        #endif
+    #else
+        #define OMD_PLATFORM_VERSION OMD_VERSION(_WRS_VXWORKS_MAJOR, 0, 0)
+    #endif
 #endif
